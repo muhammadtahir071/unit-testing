c3d96bf05f3642f0abef9360ac6680e0c50d3f1e
muhammadtahir27 <muhammad.tahir@abacuscambridge.com>
2026-07-06 15:18:11 +0500

    second commit

---
+++ apiproxy/apigee-cicd-master/scripts/integration.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# ORG=$1
+# base64encoded=$2
+echo "ORG: $ORG"
+#echo "base64encoded: $base64encoded"
+echo "ProxyName: $ProxyName"
+
+proxy_basepath=$(cat $GITHUB_WORKSPACE/apigee-cicd-master/test/integration/proxy_details.json | jq -r '.proxy_basepath')
+echo "proxy_basepath: $proxy_basepath"
+
+api_product=$(cat $GITHUB_WORKSPACE/apigee-cicd-master/test/integration/proxy_details.json | jq -r '.api_product')
+echo "api_product: $api_product"
+
+developer=$(cat $GITHUB_WORKSPACE/apigee-cicd-master/test/integration/proxy_details.json | jq -r '.developer')
+echo "developer: $developer"
+
+app=$(cat $GITHUB_WORKSPACE/apigee-cicd-master/test/integration/proxy_details.json | jq -r '.app')
+echo "app: $app"
+
+NEWMAN_TARGET_COLLECTION="${ProxyName}_${ENV}.json"
+echo "NEWMAN_TARGET_COLLECTION: $NEWMAN_TARGET_COLLECTION"
+
+ZAP_TARGET_API_URL="https://${ORG}-${ENV}.apigee.net/${proxy_basepath}"
+echo "ZAP_TARGET_API_URL: $ZAP_TARGET_API_URL"
+echo "ZAP_TARGET_API_URL=$ZAP_TARGET_API_URL" >> $GITHUB_ENV
+
+token_response=$(curl -s -X POST "https://majid-al-futtaim-group.login.apigee.com/oauth/token" -H "Content-Type:application/x-www-form-urlencoded;charset=utf-8" -H "accept: application/json;charset=utf-8" -H "authorization: Basic $SAML_BASIC" -d "grant_type=password&username=$machine_apigeeUsername&password=$machine_apigeePassword")
+
+accessToken_SAML=$(jq -r '.access_token' <<< "${token_response}")
+echo "SAML Access Token: $accessToken_SAML"
+
+client_id=$(curl -H "Authorization: Bearer $accessToken_SAML" "https://api.enterprise.apigee.com/v1/organizations/$ORG/apiproducts/$api_product?query=list&entity=keys")
+
+id=$(jq -r .[0] <<< "${client_id}" )
+#echo "client_id at script: '$id'"
+
+client_secret=$(curl -H "Authorization: Bearer $accessToken_SAML" "https://api.enterprise.apigee.com/v1/organizations/$ORG/developers/$developer/apps/$app/keys/$id")
+
+secret=$(jq -r .consumerSecret <<< "${client_secret}" )
+#echo "client_secret at script: '$secret'"
+
+sudo npm install -g newman 
+sudo npm install -g newman-reporter-htmlextra
+
+newman run $GITHUB_WORKSPACE/apigee-cicd-master/test/integration/$NEWMAN_TARGET_COLLECTION -r htmlextra --reporter-htmlextra-export ./newman_report.html --env-var client_id=$id --env-var client_secret=$secret --export-environment env.json
+
+#cat env.json
+
+accessToken=$(cat env.json | jq -r '.values[] | select(.key=="accessToken").value')
+
+#foo="${foo} World"
+# echo "accessToken at script: $accessToken"
+# echo "accessToken=$accessToken" >> $GITHUB_ENV
+
+accessToken="Bearer ${accessToken}"
+
+
+echo  "replacer.full_list(0).replacement=$accessToken" >> $GITHUB_WORKSPACE/apigee-cicd-master/zap/options.prop
+cat $GITHUB_WORKSPACE/apigee-cicd-master/zap/options.prop
+
+newman run $GITHUB_WORKSPACE/apigee-cicd-master/test/integration/$NEWMAN_TARGET_COLLECTION --reporters cli,junit --reporter-junit-export junitReport.xml --env-var client_id=$id --env-var client_secret=$secret 
+
---
+++ apiproxy/apigee-cicd-master/scripts/pullRequest_validation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "SOURCE_BRANCH_NAME:$SOURCE_BRANCH_NAME"
+echo "TARGET_BRANCH_NAME:$TARGET_BRANCH_NAME"
+        
+	if [[ "${TARGET_BRANCH_NAME}" == "dev" && "${SOURCE_BRANCH_NAME}" != "sandbox" ]];
+	then
+         {
+            echo "Cannot create Pull Request from '$SOURCE_BRANCH_NAME' to '$TARGET_BRANCH_NAME' branch...!!!"
+            exit 1
+          }
+	
+	elif [[ "${TARGET_BRANCH_NAME}" == "uat" && "${SOURCE_BRANCH_NAME}" != "dev" ]];
+	then
+         {
+            echo "Cannot create Pull Request from '$SOURCE_BRANCH_NAME' to '$TARGET_BRANCH_NAME' branch...!!!"
+            exit 1
+          }
+	
+	elif [[ "${TARGET_BRANCH_NAME}" == "prod" && "${SOURCE_BRANCH_NAME}" != "uat" ]];
+	then
+         {
+            echo "Cannot create Pull Request from '$SOURCE_BRANCH_NAME' to '$TARGET_BRANCH_NAME' branch...!!!"
+            exit 1
+          }
+		  
+	else
+	echo "Valid Pull Request generated from '$SOURCE_BRANCH_NAME' to '$TARGET_BRANCH_NAME' branch...!!!"
+	
+    fi 
